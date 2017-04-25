11de01ca6750cfbe89a291d5b27a603629f43913
Giedo Stolk <stolkie@gmail.com>
2017-04-25 12:51:51 +0200

    Yum versie check ingebouwd

--- manifests/profile/yum.pp
+++ manifests/profile/yum.pp
@@ -1,36 +1,9 @@
 # rhel6/base
 # Define all generic Linux settings and packages for RHEL6
 
-class topaas::profile::yum-rhel6 {
-	# Adding REPO
-	yumrepo { "RHEL6_CUSTOM_NS":
-		baseurl		=> "http://nsrbamtst12.nsr.dev.infosupport.net/yumrepos/RHEL6",
-		descr		=> "Custom RHEL6 Packages for NS",
-		enabled		=> 1,
-		gpgcheck	=> 0
-	}
-
-	# Packages to install
-	$install = [ 'screen',
-		           'ruby',
-		           'cifs-utils',
-		           'vim',
-		           'xinetd',
-		           'telnet',
-		           'git',
-	             'java-1.8.0-oracle.x86_64' ]
-	package {$install:
-		ensure  	=> present,
-
-
-		class topaas::profile::yum-rhel7 {
-			# Adding REPO
-			yumrepo { "RHEL7_INSTALL":
-				baseurl		=> "http://nsrbamtst12.nsr.dev.infosupport.net/iso/RHEL72/",
-				descr		=> "RHEL7 Install Packages",
-				enabled		=> 1,
-				gpgcheck	=> 0
-			}
+class topaas::profile::yum {
+	case $operatingsystemmajrelease {
+	  '6': {
 			yumrepo { "RHEL6_CUSTOM_NS":
 				baseurl		=> "http://nsrbamtst12.nsr.dev.infosupport.net/yumrepos/RHEL6",
 				descr		=> "Custom RHEL6 Packages for NS",
@@ -49,8 +22,32 @@ class topaas::profile::yum-rhel6 {
 			             'java-1.8.0-oracle.x86_64' ]
 			package {$install:
 				ensure  	=> present,
+	  }
+	  '7': {
+			# Adding REPO
+			yumrepo { "RHEL7_INSTALL":
+				baseurl		=> "http://nsrbamtst12.nsr.dev.infosupport.net/iso/RHEL72/",
+				descr		=> "RHEL7 Install Packages",
+				enabled		=> 1,
+				gpgcheck	=> 0
 			}
-		}
+			yumrepo { "RHEL6_CUSTOM_NS":
+				baseurl		=> "http://nsrbamtst12.nsr.dev.infosupport.net/yumrepos/RHEL6",
+				descr		=> "Custom RHEL6 Packages for NS",
+				enabled		=> 1,
+				gpgcheck	=> 0
+			}
 
-	}
+			# Packages to install
+			$install = [ 'screen',
+									 'ruby',
+									 'cifs-utils',
+									 'vim',
+									 'xinetd',
+									 'telnet',
+									 'git',
+									 'java-1.8.0-oracle.x86_64' ]
+			package {$install:
+				ensure  	=> present,
+	  }
 }
--- manifests/role/wlserver.pp
+++ manifests/role/wlserver.pp
@@ -1,4 +1,5 @@
 class topaas::role::wlserver {
 	class {'topaas::profile::base':}
 	class {'topaas::profile::users_groups':}
+	class {'topaas::profile::yum':}
 }
